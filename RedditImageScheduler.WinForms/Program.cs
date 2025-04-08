f2e205fbdcf920eb9c657f59cb81be026b8f6395
4as <ilovekawaiigirls@gmail.com>
2025-04-08 23:04:00 +0200

    Initial commit

---
+++ RedditImageScheduler.WinForms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace RedditImageScheduler.WinForms
+{
+	class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			new Application(Eto.Platforms.WinForms).Run(new MainForm());
+		}
+	}
+}
